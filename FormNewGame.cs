fec2a268a34501026ab5a7dad924cafe8835c78a
mohammad1998ro <unavoidableone1998@gmail.com>
2026-05-29 16:33:35 +0300

    Update project functionality and improve code structure

--- FormNewGame.cs
+++ FormNewGame.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,7 +13,7 @@ namespace RDR2_Visual_Novel
 {
     public partial class FormNewGame : Form
     {
-        private int pasPoveste = 0;
+        public int pasPoveste = 0;
         public FormNewGame()
         {
             InitializeComponent();
@@ -37,20 +37,6 @@ namespace RDR2_Visual_Novel
             // Pornim prima scenă
             IncarcaScena();
         }
-
-        private void btnSettings_Click(object sender, EventArgs e)
-        {
-            FormSettings fereastraSetari = new FormSettings();
-
-            // ShowDialog oprește timpul în FormNewGame. 
-            // Jocul așteaptă aici până când jucătorul apasă "Back" / "X" în Setări.
-            fereastraSetari.ShowDialog();
-
-            // Imediat ce setările se închid, codul continuă de aici automat!
-            // Aici vom putea aplica noile setări (ex. să dăm muzica mai încet),
-            // iar jucătorul va fi fix la replica la care rămăsese.
-        }
-
         private void btnExit_Click(object sender, EventArgs e)
         {
   
@@ -69,47 +55,69 @@ namespace RDR2_Visual_Novel
                 case 0: // Valentine
                     pbFundal.Image = Properties.Resources.valentine;
                     pbCaracter.Visible = false;
-                    lblNume.Text = "Arthur";
-                    lblText.Text = "Valentine... un oraș de noroi, oi și probleme.";
+                    lblNume.Text = "Protagonist";
+                    lblText.Text = "Am strabatut vestul salbatic ani intregi fara niciun scop.";
                     break;
 
                 case 1:
-                    lblNume.Text = "Arthur";
-                    lblText.Text = "Dar e un loc bun să faci niște bani pentru tabără.";
+                    lblNume.Text = "Protagonist";
+                    lblText.Text = "E timpul sa gasesc acel scop, sa fac parte dintr-o familie.";
                     break;
 
                 case 2: // Unchiul Dutch (Afiș)
                     pbFundal.Image = Properties.Resources.unchiu_dutch;
-                    lblNume.Text = "Arthur";
-                    lblText.Text = "Dutch are întotdeauna un plan... sper doar să funcționeze de data asta.";
+                    lblNume.Text = "Protagonist";
+                    lblText.Text = "Hmm... Am auzit de acest gang. Sper ca ma vor accepta. O sa ma plimb prin zona pana ii gasesc.";
                     break;
 
                 case 3: // Intrarea în tabără
                     pbFundal.Image = Properties.Resources.intrare;
-                    lblNume.Text = "Arthur";
-                    lblText.Text = "În sfârșit, m-am întors la Horseshoe Overlook.";
+                    lblNume.Text = "Protagonist";
+                    lblText.Text = "Dupa o ora de cautare, in sfarsit am ajuns aici.";
                     break;
 
                 case 4: // Apare Bill
                     // Fundalul ramane intrarea, dar îl afișăm pe Bill
+                    pbFundal.Image = Properties.Resources.intrare;
                     pbCaracter.Visible = true;
                     pbCaracter.Image = Properties.Resources.Bill1;
                     lblNume.Text = "Bill";
-                    lblText.Text = "Hei, englezule! Pe unde ai umblat?";
+                    lblText.Text = "Hei! Cine esti si ce cauti aici?";
                     break;
-
-                case 5: // Dutch în tabără
-                    pbFundal.Image = Properties.Resources.dutch;
-                    pbCaracter.Visible = false; // Îl ascundem pe Bill un moment
-                    lblNume.Text = "Dutch";
-                    lblText.Text = "Arthur, fiule! Avem nevoie de zgomot și de niște credite!";
-                    break;
-
-                case 6: // Apare Arthur răspundând
+                case 5:
+                    pbFundal.Image = Properties.Resources.intrare;
                     pbCaracter.Visible = true;
-                    pbCaracter.Image = Properties.Resources.IMG_2747; // Imaginea cu Arthur
+                    pbCaracter.Image = Properties.Resources.Bill1; // Imaginea cu Bill
+                    lblNume.Text = "Protagonist";
+                    lblText.Text = "Salut, sunt nou in oras. Am auzit de gangul vostru si as vrea sa ma alatur.";
+                    break;
+                case 6: 
+                    pbFundal.Image = Properties.Resources.intrare;
+                    pbCaracter.Visible = true;
+                    pbCaracter.Image = Properties.Resources.Bill1;
+                    lblNume.Text = "Bill";
+                    lblText.Text = "Hmm... Nu stiu daca Dutch te va accepta. El e destul de pretentios. Hai cu mine sa vedem.";
+                    break;
+
+                case 7: // Dutch în tabără
+                    pbFundal.Image = Properties.Resources.dutch;
+                    pbCaracter.Visible = true;
+                    pbCaracter.Image = Properties.Resources.Bill1;
+                    lblNume.Text = "Bill";
+                    lblText.Text = "Hei Dutch, tipul acesta vrea să se alăture.";
+                    break;
+
+                case 8:
+                    pbFundal.Image = Properties.Resources.dutch;
+                    pbCaracter.Visible = false;
+                    lblNume.Text = "Dutch";
+                    lblText.Text = "Interesant...Noi nu acceptam pe oricine in comunitatea noastra. Va trebui sa dovedesti ca meriti sa te alaturi.";
+                    break;
+                case 9:
+                    pbFundal.Image = Properties.Resources.harta;
+                    pbCaracter.Visible = false;
                     lblNume.Text = "Arthur";
-                    lblText.Text = "Sigur, Dutch. Dar ne cam calcă legea pe coadă.";
+                    lblText.Text = "Harta ne arată unde să căutăm următoarea țintă.";
                     break;
 
                 default: // Etapa următoare (Placeholder)
@@ -117,6 +125,22 @@ namespace RDR2_Visual_Novel
                     lblNume.Text = "Sistem";
                     lblText.Text = ">> ACESTA ESTE UN PLACEHOLDER. AICI VOR APĂREA DECIZIILE. <<";
                     break;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Salvăm valoarea variabilei pasPoveste într-un fișier numit "salvare.txt"
+                // Acest fișier se va crea singur în folderul unde rulează jocul (.exe)
+                System.IO.File.WriteAllText("salvare.txt", pasPoveste.ToString());
+
+                MessageBox.Show("Jocul a fost salvat cu succes!", "Salvare", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la salvarea jocului: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
